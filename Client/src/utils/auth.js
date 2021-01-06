fd2011770c3085c0f51ad2125101c392aa002fe6
Number3025 <mr.markus.coll@gmail.com>
2021-01-06 13:38:11 +0100

    auth0 update

--- Client/src/utils/auth.js
+++ Client/src/utils/auth.js
@@ -3,8 +3,8 @@ import history from './history';
 
 export default class Auth {
   auth0 = new auth0.WebAuth({
-    domain: 'webapp1.auth0.com',
-    clientID: 'uZxUdMAsiDWeu3OrNpoi4JwJscdF5nAx',
+    domain: 'dev-ifi9lb0f.eu.auth0.com',
+    clientID: 'gJYitdpJTFbZDU9thwdRSTxeyCnLPUdp',
     redirectUri: 'http://localhost:3000/callback',
     responseType: 'token id_token',
     scope: 'openid profile email'
